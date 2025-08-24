23cf61a70c19f1c497432ccc23b6a964e726f3f8
pedroitallo <151572811+pedroitallo@users.noreply.github.com>
2025-08-24 11:39:42 -0300

    Updated Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -24,3 +24,5 @@ export default function HomePage() {
       <div className="text-center">
         <div className="w-16 h-16 border-4 border-purple-500 border-t-transparent rounded-full animate-spin mx-auto mb-4"></div>
         <p className="text-gray-600">Loading...</p>
+  )
+}
