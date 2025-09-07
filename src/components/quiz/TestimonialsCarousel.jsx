afd9588bab8a168d720c00e7bc85a0156fd79977
pedroitallo <151572811+pedroitallo@users.noreply.github.com>
2025-09-07 15:12:48 -0300

    Updated TestimonialsCarousel.jsx

--- src/components/quiz/TestimonialsCarousel.jsx
+++ src/components/quiz/TestimonialsCarousel.jsx
@@ -290,7 +290,7 @@ export default function TestimonialsCarousel({ onContinue }) {
                 />
                 <div className="text-left">
                   <p className="text-base text-gray-700 leading-relaxed">
-                    My name is Aura, and I became famous in 2024 as <strong> Hollywood’s #1 phychic</strong> , uniting soulmates through my drawings, including <strong> Beyonce and Taylor Swift.</strong> 👇🏼
+                    My name is Aura, and I became famous in 2024 as <strong> Hollywood’s #1 phychic</strong> , uniting soulmates through my drawings👇🏼
                   </p>
                 </div>
               </div>
