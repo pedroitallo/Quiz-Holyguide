6a151e75d6f033b285c1bb5aa2ede59009d0190c
pedroitallo <151572811+pedroitallo@users.noreply.github.com>
2025-09-06 22:08:26 -0300

    Updated TestimonialsCarousel.jsx

--- src/components/quiz/TestimonialsCarousel.jsx
+++ src/components/quiz/TestimonialsCarousel.jsx
@@ -50,7 +50,7 @@ const ScrollIndicator = () => {
 
 const testimonials = [
 {
-  imageUrl: "https://base44.app/api/apps/68850befb229de9dd8e4dc73/files/14ed61e13_DEP1.webp"
+  imageUrl: "https://base44.app/api/apps/68850befb229de9dd8e4dc73/files/public/68850befb229de9dd8e4dc73/84336022e_CapturadeTela2025-09-06as190035.png"
 },
 {
   imageUrl: "https://base44.app/api/apps/68850befb229de9dd8e4dc73/files/e858f01a6_DEP2.webp"
