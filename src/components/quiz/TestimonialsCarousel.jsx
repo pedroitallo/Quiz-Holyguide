460abc66e1171b8c568706cd84cf6d1aca9528c7
pedroitallo <151572811+pedroitallo@users.noreply.github.com>
2025-09-06 22:08:56 -0300

    Updated TestimonialsCarousel.jsx

--- src/components/quiz/TestimonialsCarousel.jsx
+++ src/components/quiz/TestimonialsCarousel.jsx
@@ -53,7 +53,7 @@ const testimonials = [
   imageUrl: "https://base44.app/api/apps/68850befb229de9dd8e4dc73/files/public/68850befb229de9dd8e4dc73/7701cbbfc_CapturadeTela2025-09-06as190013.png"
 },
 {
-  imageUrl: "https://base44.app/api/apps/68850befb229de9dd8e4dc73/files/public/68850befb229de9dd8e4dc73/7701cbbfc_CapturadeTela2025-09-06as190013.png"
+  imageUrl: "https://base44.app/api/apps/68850befb229de9dd8e4dc73/files/public/68850befb229de9dd8e4dc73/84336022e_CapturadeTela2025-09-06as190035.png"
 },
 {
   imageUrl: "https://base44.app/api/apps/68850befb229de9dd8e4dc73/files/14ed61e13_DEP1.webp"
