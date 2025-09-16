8aa711f1cdb288d48be47a10c78eaa242d2306dc
pedroitallo <151572811+pedroitallo@users.noreply.github.com>
2025-09-16 18:17:51 -0300

    Fix duplicate Helmet import in Layout.jsx

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect } from 'react';
 import { Helmet } from 'react-helmet';
-import { Helmet } from 'react-helmet';
 
 export default function Layout({ children, currentPageName }) {
   useEffect(() => {
