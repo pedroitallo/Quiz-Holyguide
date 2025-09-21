b93fbf42f52966a8fd208d1c733071212faba81f
pedroitallo <151572811+pedroitallo@users.noreply.github.com>
2025-09-21 14:00:06 -0300

    Add Meta Pixel Code to head

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -23,6 +23,27 @@ export default function Layout({ children, currentPageName }) {
     <div style={{ userSelect: 'none', WebkitUserSelect: 'none', MozUserSelect: 'none', msUserSelect: 'none' }}>
       <Helmet>
         <script type="text/javascript" src="https://tkk.holyguide.online/track.js"></script>
+        {/* Meta Pixel Code */}
+        <script dangerouslySetInnerHTML={{
+          __html: `
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '1339007620920011');
+            fbq('track', 'PageView');
+          `
+        }} />
+        <noscript>
+          <img height="1" width="1" style={{display:'none'}}
+            src="https://www.facebook.com/tr?id=1339007620920011&ev=PageView&noscript=1"
+          />
+        </noscript>
+        {/* End Meta Pixel Code */}
       </Helmet>
       <style>{`
         /* Critical CSS inline para melhorar FCP */
