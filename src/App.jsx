367e1078c851ed200325e91770d11537bafdaef9
pedroitallo <151572811+pedroitallo@users.noreply.github.com>
2025-10-04 18:16:37 -0300

    Updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import './App.css'
 import Pages from "@/pages/index.jsx"
-import { Toaster } from "@/components/ui/toaster"
 
 function App() {
   return (
     <>
       <Pages />
-      <Toaster />
     </>
   )
 }
