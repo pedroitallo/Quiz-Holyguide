c6ee3b3d4aee018d36172c43f83ed7e129b5f896
pedroitallo <151572811+pedroitallo@users.noreply.github.com>
2025-10-05 00:26:09 -0300

    Updated IntroStep.jsx

--- src/components/quiz/funnel-star3/IntroStep.jsx
+++ src/components/quiz/funnel-star3/IntroStep.jsx
@@ -21,7 +21,7 @@ export default function IntroStep({ onContinue }) {
         />
       </div>
 
-      <p className="text-black text-lg md:text-xl leading-relaxed max-w-3xl mx-auto px-4">
+      <p className="text-black text-lg md:text-xl leading-relaxed max-w-3xl mx-auto px-2">
         More Than 10,000 People Have Found Their Soul Mate After This 1-Minute Astral Test
       </p>
       <Button
