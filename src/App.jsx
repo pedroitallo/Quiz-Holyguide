fccadcf312dbc98c96693b20593ec38bb2d578ef
pedroitallo <151572811+pedroitallo@users.noreply.github.com>
2025-10-06 15:28:11 -0300

    Updated package-lock.json

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
 import Pages from "@/pages/index.jsx"
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 function App() {
   return (
     <>
       <Pages />
+      <SpeedInsights />
     </>
   )
 }
