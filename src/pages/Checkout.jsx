052501a0aca79b99e62a0bbc27e7f1bc15125e15
pedroitallo <151572811+pedroitallo@users.noreply.github.com>
2025-10-16 21:05:37 -0300

    Updated Checkout.jsx

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -27,8 +27,8 @@ export default function Checkout() {
             content: '';
             position: absolute;
             top: 230px;
-            left: 30px;
-            right: 30px;
+            left: 50px;
+            right: 50px;
             height: 80px;
             background: white;
             z-index: 10;
