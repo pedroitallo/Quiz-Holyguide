4b02beff18816363caa940eec6639fc89c462a12
pedroitallo <151572811+pedroitallo@users.noreply.github.com>
2025-11-17 20:10:34 -0300

    Updated Checkout.jsx

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -38,7 +38,7 @@ export default function Checkout() {
           <div
             className="
               absolute left-1/2 -translate-x-1/2 z-20
-              w-[min(95vw,800px)] h-[70px]
+              w-[min(95vw,800px)] h-[110px]
               top-[230px]
               bg-white rounded-xl pointer-events-none
             "
